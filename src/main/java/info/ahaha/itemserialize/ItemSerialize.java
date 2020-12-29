f48457768f6a7f0dbdce1dc269ead5aad9e3c15a
aruru-weed <aruzenyukkuri@gmail.com>
2020-12-29 19:20:00 +0900

    1.16.4

--- src/main/java/info/ahaha/itemserialize/ItemSerialize.java
+++ src/main/java/info/ahaha/itemserialize/ItemSerialize.java
@@ -1,7 +1,11 @@
 package info.ahaha.itemserialize;
 
 import com.google.common.io.BaseEncoding;
+import net.minecraft.server.v1_16_R3.NBTCompressedStreamTools;
+import net.minecraft.server.v1_16_R3.NBTTagCompound;
+import net.minecraft.server.v1_16_R3.NBTTagList;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.*;
@@ -10,60 +14,46 @@ import java.lang.reflect.InvocationTargetException;
 import java.math.BigInteger;
 
 public final class ItemSerialize {
-    public static ItemStack deserializeItemStack(String data) {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(new BigInteger(data, 32).toByteArray());
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-
-        ItemStack itemStack = null;
-        try {
-            Class<?> nbtTagCompoundClass = getNMSClass("NBTTagCompound");
-            Class<?> nmsItemStackClass = getNMSClass("ItemStack");
-            Object nbtTagCompound = getNMSClass("NBTCompressedStreamTools").getMethod("a", InputStream.class).invoke(null, dataInputStream);
-            Object craftItemStack = nmsItemStackClass.getMethod("createStack", nbtTagCompoundClass).invoke(null, nbtTagCompound);
-            itemStack = (ItemStack) getOBClass("inventory.CraftItemStack").getMethod("asBukkitCopy", nmsItemStackClass).invoke(null, craftItemStack);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-            e.printStackTrace();
-        }
-
-        return itemStack;
-    }
-
     public static String serializeItemStack(ItemStack item) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         DataOutputStream dataOutput = new DataOutputStream(outputStream);
 
-        try {
-            Class<?> nbtTagCompoundClass = getNMSClass("NBTTagCompound");
-            Constructor<?> nbtTagCompoundConstructor = nbtTagCompoundClass.getConstructor();
-            Object nbtTagCompound = nbtTagCompoundConstructor.newInstance();
-            Object nmsItemStack = getOBClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-            getNMSClass("ItemStack").getMethod("save", nbtTagCompoundClass).invoke(nmsItemStack, nbtTagCompound);
-            getNMSClass("NBTCompressedStreamTools").getMethod("a", nbtTagCompoundClass, DataOutput.class).invoke(null, nbtTagCompound, (DataOutput) dataOutput);
+        NBTTagList nbtTagListItems = new NBTTagList();
+        NBTTagCompound nbtTagCompoundItem = new NBTTagCompound();
 
-        } catch (SecurityException | NoSuchMethodException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        net.minecraft.server.v1_16_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+
+        nmsItem.save(nbtTagCompoundItem);
+
+        nbtTagListItems.add(nbtTagCompoundItem);
+
+        try {
+            NBTCompressedStreamTools.a(nbtTagCompoundItem, (DataOutput) dataOutput);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-//        return BaseEncoding.base64().encode(outputStream.toByteArray());
+
         return new BigInteger(1, outputStream.toByteArray()).toString(32);
     }
 
-    private static Class<?> getNMSClass(String name) {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-        try {
-            return Class.forName("net.minecraft.server." + version + "." + name);
-        } catch (ClassNotFoundException var3) {
-            var3.printStackTrace();
-            return null;
-        }
-    }
+    /**
+     * Item from Base64
+     *
+     * @param data
+     * @return
+     */
+    public static ItemStack deserializeItemStack(String data) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(new BigInteger(data, 32).toByteArray());
 
-    private static Class<?> getOBClass(String name) {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        NBTTagCompound nbtTagCompoundRoot = null;
         try {
-            return Class.forName("org.bukkit.craftbukkit." + version + "." + name);
-        } catch (ClassNotFoundException var3) {
-            var3.printStackTrace();
-            return null;
+            nbtTagCompoundRoot = NBTCompressedStreamTools.a((DataInput) new DataInputStream(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        net.minecraft.server.v1_16_R3.ItemStack nmsItem = net.minecraft.server.v1_16_R3.ItemStack.a(nbtTagCompoundRoot);
+        ItemStack item = (ItemStack) CraftItemStack.asBukkitCopy(nmsItem);
+        return item;
     }
 }
