7341d033a1725ceb2fdd7e800f6f96fb9f950bfd
aruru-weed <aruzenyukkuri@gmail.com>
2021-10-15 20:33:01 +0900

    1.17

--- src/main/java/info/ahaha/itemserialize/ItemSerialize.java
+++ src/main/java/info/ahaha/itemserialize/ItemSerialize.java
@@ -1,9 +1,9 @@
 package info.ahaha.itemserialize;
 
-import net.minecraft.server.v1_16_R3.NBTCompressedStreamTools;
-import net.minecraft.server.v1_16_R3.NBTTagCompound;
-import net.minecraft.server.v1_16_R3.NBTTagList;
-import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
+import net.minecraft.nbt.NBTCompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.*;
@@ -17,7 +17,7 @@ public final class ItemSerialize {
         NBTTagList nbtTagListItems = new NBTTagList();
         NBTTagCompound nbtTagCompoundItem = new NBTTagCompound();
 
-        net.minecraft.server.v1_16_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
 
         nmsItem.save(nbtTagCompoundItem);
 
@@ -32,12 +32,6 @@ public final class ItemSerialize {
         return new BigInteger(1, outputStream.toByteArray()).toString(32);
     }
 
-    /**
-     * Item from Base64
-     *
-     * @param data
-     * @return
-     */
     public static ItemStack deserializeItemStack(String data) {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(new BigInteger(data, 32).toByteArray());
 
@@ -48,8 +42,7 @@ public final class ItemSerialize {
             e.printStackTrace();
         }
 
-        net.minecraft.server.v1_16_R3.ItemStack nmsItem = net.minecraft.server.v1_16_R3.ItemStack.a(nbtTagCompoundRoot);
-        ItemStack item = (ItemStack) CraftItemStack.asBukkitCopy(nmsItem);
-        return item;
+        net.minecraft.world.item.ItemStack nmsItem = net.minecraft.world.item.ItemStack.a(nbtTagCompoundRoot);
+        return CraftItemStack.asBukkitCopy(nmsItem);
     }
 }
